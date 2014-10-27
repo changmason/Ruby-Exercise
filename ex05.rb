c7748f0b69242092b823c9ee0ff51790eab82365
Ericlee0210 <imdfmy@gmail.com>
2014-10-27 13:53:31 +0800

    10/27 update from ex04~ex10

---
+++ ex05.rb
@@ -0,0 +1,16 @@
+my_name = 'Eric'
+my_age = 21 # not a lie in 2009
+my_height = 179 # cm
+my_weight = 70 # kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} cm tall."
+puts "He's #{my_weight} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are #{my_teeth}.."
+
+puts "If I add #{my_age}, #{my_height}, #{my_weight}, I got #{my_age+my_height+my_weight}"
